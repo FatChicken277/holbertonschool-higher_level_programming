7a2de7656e27b3cd4f37b78cf10212d35ec86b7e
FatChicken277 <1637@holbertonschool.com>
2020-04-28 14:40:01 -0500

    remake code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 for first_digit in range(10):
-    for second_digit in range(10):
-        if second_digit != first_digit:
-            if first_digit < second_digit:
-                print(first_digit, end="")
-                if first_digit == 8 and second_digit == 9:
-                    print(second_digit)
-                else:
-                    print(second_digit, end=", ")
+    for second_digit in range(first_digit+1, 10):
+        if first_digit == 8 and second_digit == 9:
+            print("{}{}".format(first_digit, second_digit))
+        else:
+            print("{}{}".format(first_digit, second_digit), end=", ")
