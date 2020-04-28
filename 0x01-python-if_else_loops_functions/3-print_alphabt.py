6a77e70298783f5e3d39398d02658078715d4aa0
FatChicken277 <1637@holbertonschool.com>
2020-04-28 12:18:25 -0500

    remove chr function

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 122):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print(chr(i), end='\0')
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
