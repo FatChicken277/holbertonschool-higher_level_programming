467a3dc3f689ac59876f0d1d775e8b7bf77f88e6
FatChicken277 <1637@holbertonschool.com>
2020-04-30 14:40:49 -0500

    fixed variables values

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,8 +8,8 @@ def calculator(args):
     if lenn != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(args[3])
-    b = int(args[1])
+    a = int(args[1])
+    b = int(args[3])
     if args[2] == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif args[2] == '-':
