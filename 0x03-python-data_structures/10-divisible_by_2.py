fca9bfbaf32a8fe6969aca50e06e4c844053321d
FatChicken277 <1637@holbertonschool.com>
2020-05-05 16:04:07 -0500

    fixed conditional

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list or len(my_list) != None:
+    if my_list or len(my_list) != 0:
         new_list = my_list.copy()
         for i in range(len(new_list)):
             if new_list[i] % 2 == 0:
