3377a403e58f3a5175393f47954de72bcd0dfc5c
FatChicken277 <1637@holbertonschool.com>
2020-05-18 13:37:43 -0500

    added 101-safe_function.py file

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    a = None
+    try:
+        a = fct(*args)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+    return a
