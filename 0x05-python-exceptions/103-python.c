dc9174414660be1f39f31b948b975b67e440e6dd
FatChicken277 <1637@holbertonschool.com>
2020-05-18 14:48:53 -0500

    added 103-python.c file

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,93 @@
+#include <Python.h>
+#include <object.h>
+#include <bytesobject.h>
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - print info
+ * @p: object
+ */
+
+void print_python_list(PyObject *p)
+{
+
+	long int len;
+	int i;
+	const char *type;
+
+	len = ((PyVarObject *)p)->ob_size;
+	PyListObject *aux = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	if (PyList_Check(p))
+	{
+		printf("[*] Size of the Python List = %li\n", len);
+		printf("[*] Allocated = %li\n", aux->allocated);
+
+		for (i = 0; i < len; i++)
+		{
+			type = aux->ob_item[i]->ob_type->tp_name;
+			printf("Element %d: %s\n", i, type);
+			if (strcmp(type, "bytes") == 0)
+				print_python_bytes(aux->ob_item[i]);
+			if (strcmp(type, "float") == 0)
+				print_python_float(aux->ob_item[i]);
+		}
+	}
+	else
+	{
+		printf("  [ERROR] Invalid List Object\n");
+	}
+}
+
+/**
+ * print_python_bytes - print info
+ * @p: object
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	int size, i;
+	PyBytesObject *aux = (PyBytesObject *)p;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p))
+	{
+		printf("  size: %ld\n", PyBytes_Size(p));
+		printf("  trying string: %s\n", aux->ob_sval);
+
+		if (((PyVarObject *)p)->ob_size > 10)
+			size = 10;
+		else
+			size = ((PyVarObject *)p)->ob_size + 1;
+		printf("  first %d bytes: ", size);
+		for (i = 0; i < size; i++)
+		{
+			printf("%02hhx", aux->ob_sval[i]);
+			if (i + 1 == size)
+				printf("\n");
+			else
+				printf(" ");
+		}
+	}
+	else
+		printf("  [ERROR] Invalid Bytes Object\n");
+}
+
+/**
+ * print_python_float - print info
+ * @p: object
+ */
+void print_python_float(PyObject *p)
+{
+	PyFloatObject *aux = (PyFloatObject *)p;
+
+	printf("[.] float object info\n");
+	if (PyFloat_Check(p))
+	{
+		printf("  Value: %f\n", aux->ob_fval);
+	}
+	else
+		printf("  [ERROR] Invalid Float Object\n");
+}
