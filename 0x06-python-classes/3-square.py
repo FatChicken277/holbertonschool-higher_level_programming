675fc3f7aa3ebe3ec7eaa8d74e18d4ae0431ba5d
FatChicken277 <1637@holbertonschool.com>
2020-05-19 18:12:35 -0500

    added 3-square.py file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Create and define the class "Square".
+
+This module creates an class called "Square" that defines a square.
+
+  Typical usage example:
+
+  var = Square() or var = Square(arg)
+"""
+
+
+class Square:
+    """Defines a Square.
+
+    Defines a square and its values subject to certain conditions.
+
+    Attributes:
+        __size: size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Inits Square with a size subject to certain conditions."""
+        if size != int(size):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
