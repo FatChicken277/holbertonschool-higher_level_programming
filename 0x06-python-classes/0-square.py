7fdc6419a6ea7ef02106526603dfff765273bb1d
FatChicken277 <1637@holbertonschool.com>
2020-05-19 16:52:41 -0500

    added 0-square.py file

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Create a empty class "Square".
+
+This module creates an empty class called "Square" that defines a square.
+
+  Typical usage example:
+
+  var = Square()
+"""
+
+
+class Square:
+    """empty class"""
+    pass
