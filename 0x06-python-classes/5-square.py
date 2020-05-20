126fc03b9edc5bb6401220c3a8d86ee816843f1d
FatChicken277 <1637@holbertonschool.com>
2020-05-19 21:42:20 -0500

    fixed errors

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,7 +22,7 @@ class Square:
     def __init__(self, size=0):
         """Inits Square."""
         if type(size) is not int:
-            raise ValueError("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
@@ -36,7 +36,7 @@ class Square:
     def size(self, value):
         """Sets Square with a size subject to certain conditions."""
         if type(value) is not int:
-            raise ValueError("size must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
