a0965a0ffcf4dc2935844465c8c2d7938277ecef
FatChicken277 <1637@holbertonschool.com>
2020-05-19 21:26:21 -0500

    fixed init conditionals

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,7 +22,15 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Inits Square."""
+        if type(size) is not int:
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+        if type(position) is not tuple or type(position[0]) is not int\
+                or type(position[1]) is not int\
+                or position[0] < 0 or position[1] < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     @property
@@ -50,6 +58,7 @@ class Square:
         if type(value) is not tuple or type(value[0]) is not int\
                 or type(value[1]) is not int or value[0] < 0 or value[1] < 0:
             raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Returns the current square area."""
