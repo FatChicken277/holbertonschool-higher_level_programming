44d844abb8540c75b0b9004c4b465916446fb0bc
FatChicken277 <1637@holbertonschool.com>
2020-05-19 23:08:18 -0500

    remove init conditionals

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,26 +24,10 @@ class Square:
         """Inits Square.
 
         Keyword Arguments:
-            size {Int} -- size (default: {0})
-            position {Tuple} -- position (default: {(0, 0)})
-
-        Raises:
-            TypeError: size must be an integer
-            ValueError: size must be >= 0
-            TypeError: position must be a tuple of 2 positive integers
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
+            size {int} -- size (default: {0})
+            position {tuple} -- position (default: {(0, 0)})
+        """        
             self.__size = size
-        if type(position) is not tuple or len(position) is not 2\
-                or type(position[0]) is not int\
-                or type(position[1]) is not int\
-                or position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
             self.__position = position
 
     @property
