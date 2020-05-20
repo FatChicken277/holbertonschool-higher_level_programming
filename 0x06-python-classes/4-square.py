ba52c50530b79482f44068155ecc1b39a6d137df
FatChicken277 <1637@holbertonschool.com>
2020-05-19 19:01:06 -0500

    added 4-square.py file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Create and define the class "Square".
+
+This module creates an class called "Square" that defines a square.
+
+  Typical usage example:
+
+  var = Square() or var = Square(arg)
+"""
+
+
+class Square:
+    """Defines a Square.
+
+    Defines a square and its values subject to certain conditions.
+
+    Attributes:
+        __size: size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Inits Square."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Returns Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets Square with a size subject to certain conditions."""
+        if type(value) is not int:
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
