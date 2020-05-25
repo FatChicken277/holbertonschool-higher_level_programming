1cdad196cd45d0206e96932a1093fe15c4eda0af
FatChicken277 <1637@holbertonschool.com>
2020-05-25 18:43:06 -0500

    fixed return

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -153,6 +153,8 @@ class Rectangle:
         else:
             if rect_1.area() >= rect_2.area():
                 return rect_1
+            else:
+                return rect_2
 
     @classmethod
     def square(cls, size=0):
