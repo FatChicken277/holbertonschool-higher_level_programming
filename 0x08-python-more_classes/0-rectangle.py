428e32c74e78f7d066de29dd0532feb93fe2db75
FatChicken277 <1637@holbertonschool.com>
2020-05-25 17:21:48 -0500

    added 0-rectangle.py file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module contains a empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """Empy class that defines a rectangle.
+    """
+    pass
