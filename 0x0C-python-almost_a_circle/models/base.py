315457707c5b430fc0fcd6a012a4f77ee89bffad
FatChicken277 <1637@holbertonschool.com>
2020-06-09 19:01:23 -0500

    added advanced tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 import json
 import turtle
 import time
+import csv
 
 
 class Base:
@@ -98,6 +99,64 @@ class Base:
             return []
         return [cls.create(**i) for i in pylist]
 
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writes the csv string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): A list of instances who inherits of Base.
+        """
+        li = []
+        filename = "{}.csv".format(cls.__name__)
+        if list_objs is not None:
+            for obj in list_objs:
+                li.append(obj.to_dictionary())
+        with open(filename, mode="w", encoding="utf-8") as file:
+            csvfile = csv.writer(file)
+            for i in li:
+                a = []
+                if cls.__name__ == "Rectangle":
+                    menu = ["id", "width", "height", "x", "y"]
+                    for x in menu:
+                        a.append(i[x])
+                elif cls.__name__ == "Square":
+                    menu = ["id", "size", "x", "y"]
+                    for x in menu:
+                        a.append(i[x])
+                csvfile.writerow(a)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Returns a list of instances.
+
+        Returns:
+            list: list of instances.
+        """
+        filename = "{}.csv".format(cls.__name__)
+        try:
+            with open(filename, mode="r", encoding="utf-8") as file:
+                reader = list(csv.reader(file, delimiter=','))
+                if cls.__name__ == "Rectangle":
+                    menu = ["id", "width", "height", "x", "y"]
+                    lista = []
+                    for i in reader:
+                        newdict = {}
+                        for idx, x in enumerate(menu):
+                            newdict[x] = int(i[idx])
+                        lista.append(newdict)
+                    return [cls.create(**i) for i in lista]
+                elif cls.__name__ == "Square":
+                    menu = ["id", "size", "x", "y"]
+                    lista = []
+                    for i in reader:
+                        newdict = {}
+                        for idx, x in enumerate(menu):
+                            newdict[x] = int(i[idx])
+                        lista.append(newdict)
+                    return [cls.create(**i) for i in lista]
+        except Exception:
+            return []
+
     @staticmethod
     def draw(list_rectangles, list_squares):
         """Draw all rectangles and squares
@@ -107,19 +166,26 @@ class Base:
             list_squares (list): list of squares.
         """
         t = turtle.Turtle()
-        style = ("Arial", 11, 'normal', 'bold')
+        style = ("Arial", 15, 'normal', 'bold')
+        s2 = ("Arial", 5, 'normal')
+        t.speed(200)
         for i in list_rectangles:
             if "rectangle" in str(type(i)):
+                t.shape("circle")
+                t.shapesize(0.3, 0.01, 0.2)
                 t.penup()
+                t.setposition(0, 280)
+                t.color('#4a661a')
+                t.write("[Type] (Id) X/Y - Wi/He", font=style, align="center")
+                t.setposition(0, 250)
+                t.write(i, font=style, align="center")
                 x = i.x
                 y = i.y
                 h = i.height
                 w = i.width
-                t.setposition(x+w/2, y-30)
-                t.color('#4a661a')
-                t.write(i, font=style, align="center")
                 t.color('black')
                 t.setposition(x, y)
+                t.speed(3)
                 t.pendown()
                 t.fillcolor("#4a661a")
                 t.begin_fill()
@@ -129,19 +195,41 @@ class Base:
                     t.forward(h)
                     t.left(90)
                 t.end_fill()
-                time.sleep(2)
+                t.penup()
+                t.goto(0, 0)
+                t.pendown()
+                t.speed(200)
+                for x in range(4):
+                    c = 0
+                    for j in range(10):
+                        t.write(str(c), align="center", font=s2)
+                        if x == 3 or x == 2:
+                            c -= 20
+                        else:
+                            c += 20
+                        t.stamp()
+                        t.forward(20)
+                    t.goto(0, 0)
+                    t.left(90)
+                time.sleep(3)
                 turtle.resetscreen()
         for i in list_squares:
             if "square" in str(type(i)):
+                t.shape("circle")
+                t.shapesize(0.3, 0.01, 0.2)
+                t.penup()
+                t.setposition(0, 280)
+                t.color('#c75038')
+                t.write("[Type] (Id) X/Y - Wi/He", font=style, align="center")
+                t.setposition(0, 250)
+                t.write(i, font=style, align="center")
                 t.penup()
                 s = i.size
                 x = i.x
                 y = i.y
-                t.setposition(x+s/2, y-30)
-                t.color('#c75038')
-                t.write(i, font=style, align="center")
                 t.color('black')
                 t.setposition(x, y)
+                t.speed(3)
                 t.pendown()
                 t.fillcolor("#c75038")
                 t.begin_fill()
@@ -149,5 +237,21 @@ class Base:
                     t.forward(s)
                     t.left(90)
                 t.end_fill()
-                time.sleep(2)
+                t.penup()
+                t.goto(0, 0)
+                t.pendown()
+                t.speed(200)
+                for x in range(4):
+                    c = 0
+                    for j in range(10):
+                        t.write(str(c), align="center", font=s2)
+                        if x == 3 or x == 2:
+                            c -= 20
+                        else:
+                            c += 20
+                        t.stamp()
+                        t.forward(20)
+                    t.goto(0, 0)
+                    t.left(90)
+                time.sleep(3)
                 turtle.resetscreen()
