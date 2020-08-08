958892d71384a719e659dce9290e12952bb9d559
FatChicken277 <1637@holbertonschool.com>
2020-08-08 13:46:11 -0500

    added conditional

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -42,10 +42,10 @@ def query(cur):
     """Performs a "SELECT" query that lists all states with a name starting
         with N
     """
-    cur.execute("""SELECT * FROM states WHERE name REGEXP
-                '^N.*' ORDER BY id ASC""")
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
     for row in cur.fetchall():
-        print(row)
+        if row[1][0] == "N":
+            print(row)
 
 
 if __name__ == "__main__":
