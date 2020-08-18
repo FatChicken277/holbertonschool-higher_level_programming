5558805b878ddc22edefd2aa2870b4f0b7cd8a85
FatChicken277 <1637@holbertonschool.com>
2020-08-18 14:21:45 -0500

    remove conditional

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,4 @@
 # This script sends a request to a URL, and displays the
 # size of the body of the response.
 
-if [ "$#" -eq 1 ]; then
-    curl -sI "$1" | awk -v Del=": " '/^Content-Length/{print $2}'
-fi
+curl -sI "$1" | awk -v Del=": " '/^Content-Length/{print $2}'
