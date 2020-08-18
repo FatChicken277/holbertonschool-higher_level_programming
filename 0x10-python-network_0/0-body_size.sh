a6df21acc39eeccab137b3b902745541ed9c055a
FatChicken277 <1637@holbertonschool.com>
2020-08-18 14:28:12 -0500

    remove unnecessary lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL, and displays the
-# size of the body of the response.
-
-curl -sI "$1" | awk -v Del=": " '/^Content-Length/{print $2}'
+# This script the size of the body of the response
+curl -sI "$1" | awk '/^Content-Length/{print $2}'
