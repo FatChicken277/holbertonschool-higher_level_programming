bf28a9737b3e65d19f281d2ed826502cfb7f0ccd
FatChicken277 <1637@holbertonschool.com>
2020-08-18 14:43:39 -0500

    updated flags

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a DELETE request to the URL passed as the first argument and displays the body of the response.
-curl -X DELETE "$1"
+# This script sends a DELETE request to a URL.
+curl -sX DELETE "$1"
