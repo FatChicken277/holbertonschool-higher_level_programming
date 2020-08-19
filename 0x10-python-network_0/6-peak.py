e41c455b2d2a5af00e106e31fdd072fc75bef4b5
FatChicken277 <1637@holbertonschool.com>
2020-08-18 19:28:10 -0500

    try with sort method

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,4 +8,5 @@ def find_peak(list_of_integers):
     """That finds a peak in a list of unsorted integers.
     """
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
