20a0cb5afda86f0189da6c6f4f70c06a754ae28f
FatChicken277 <1637@holbertonschool.com>
2020-08-24 17:18:54 -0500

    remove x variable

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,10 +8,6 @@ from sys import argv
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
     r = requests.get(url)
-    x = 0
-    for i in r.json():
-        x += 1
-        if x == 11:
-            break
+    for i in r.json()[:10]:
         print("{}: {}".format(
             i.get('sha'), i.get('commit').get('author').get('name')))
