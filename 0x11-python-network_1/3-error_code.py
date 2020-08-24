c3b4202b405fd11d2a2404a552411d09f267d27c
FatChicken277 <1637@holbertonschool.com>
2020-08-24 15:17:03 -0500

    added 3-error_code.py file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL and displays
+    the body of the response. (Catching errors)
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
+    else:
+        print(response.read().decode('utf-8'))
