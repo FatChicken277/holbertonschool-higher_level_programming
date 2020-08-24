163445f3e07cd069aaf1e207337d015e295c4f86
FatChicken277 <1637@holbertonschool.com>
2020-08-24 17:09:22 -0500

    added 100-github_commits.py file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""[summary]
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    r = requests.get(url)
+    x = 0
+    for i in r.json():
+        x += 1
+        if x == 9:
+            break
+        print("{}: {}".format(i.get('sha'), i.get('author').get('login')))
