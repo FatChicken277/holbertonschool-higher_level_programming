0aad9e6025bdcf01bc4bafd18242ae39000e5f7e
FatChicken277 <1637@holbertonschool.com>
2020-09-15 13:09:47 -0500

    Replaced forEach to for loop

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,11 @@ if (!process.argv[2] || process.argv.length === 3) {
 } else {
   let second = parseInt(process.argv[2]);
   let first = parseInt(process.argv[2]);
-  process.argv.slice(2).forEach(val => {
-    if (parseInt(val) > first) {
+  for (let i = 2; i < process.argv.length - 1; i++) {
+    if (process.argv[i] > first) {
       second = first;
-      first = parseInt(val);
+      first = parseInt(process.argv[i]);
     }
-  });
+  }
   console.log(second);
 }
