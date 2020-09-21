378a48d0fdc3d575404589fb18f40c85654e4ee3
FatChicken277 <1637@holbertonschool.com>
2020-09-21 16:49:41 -0500

    change const to let

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const dictionary = require('./101-main').dict;
-const newDict = {};
+let newDict = {};
 for (const [key, value] of Object.entries(dictionary)) {
   if (newDict[value] !== undefined) {
     newDict[value].push(key);
