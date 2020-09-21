1976a894227dd749bdb895868d7fbaed6f58ba53
FatChicken277 <1637@holbertonschool.com>
2020-09-21 17:50:33 -0500

    fixed filename

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dictionary = require('101-data.js').dict;
+const dictionary = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dictionary) {
   if (newDict[dictionary[key]] === undefined) {
