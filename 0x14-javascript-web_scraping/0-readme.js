044a8e1334191efb6f9de2bb650f425d9fbb8c27
FatChicken277 <1637@holbertonschool.com>
2020-09-22 17:07:04 -0500

    added 0-readme.js file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.argv.length === 3) {
+  const fs = require('fs');
+  try {
+    console.log(fs.readFileSync(process.argv[2]).toString('utf8'));
+  } catch (err) {
+    console.log(err);
+  }
+}
