d6254e5c6a54a76b8c6a9d265b0e1c0921ff2174
FatChicken277 <1637@holbertonschool.com>
2020-09-23 00:45:59 -0500

    remove the last change and remove else

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 if (process.argv.length === 3) {
   const request = require('request');
-  request(`${process.argv[2]}?completed=true`, function (err, res, body) {
+  request(process.argv[2], function (err, res, body) {
     if (res.statusCode === 200) {
       const users = JSON.parse(body);
       const usersDict = {};
       for (const user of users) {
         if (usersDict[user.userId] === undefined) {
-          usersDict[user.userId] = 1;
-        } else {
+          usersDict[user.userId] = 0;
+        }
+        if (user.completed === true) {
           usersDict[user.userId] += 1;
         }
       }
